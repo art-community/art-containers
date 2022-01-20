baffee28107b8e8571db005e6a692c97d3058c33
anton.bashirov <anton.sh.local@gmail.com>
2022-01-20 06:30:58 +0300

    ART

--- container/src/main/kotlin/io/art/linux/local/container/Main.kt
+++ container/src/main/kotlin/io/art/linux/local/container/Main.kt
@@ -1,25 +1,31 @@
 package io.art.linux.local.container
 
 import io.art.configurator.kotlin.configurator
+import io.art.core.checker.TerminalChecker.terminalSupportColors
 import io.art.launcher.kotlin.activator
 import io.art.linux.local.container.graal.GraalLxcProvider
 import io.art.linux.local.container.graal.GraalLxcProvider.getGlobalConfiguration
-import io.art.logging.kotlin.logger
-import io.art.logging.kotlin.logging
+import io.art.logging.colorizer.AnsiColorizer.additional
+import io.art.logging.colorizer.AnsiColorizer.success
 import io.art.scheduler.kotlin.scheduler
 import io.art.transport.kotlin.transport
 
+fun log(message: String) = when {
+    terminalSupportColors() -> {
+        println(additional("(art.local): ") + success(message))
+    }
+    else -> {
+        println("(art.local): $message")
+    }
+}
+
 fun main() = activator {
-    logging()
     transport()
     scheduler()
     configurator()
-    silent(true)
     onLaunch {
-        logger {
-            info("Hello, ART with lxc: ${GraalLxcProvider.version()}")
-            info("LXC Value: ${getGlobalConfiguration("lxc.lxcpath")}")
-        }
+        log("Hello, ART with lxc: ${GraalLxcProvider.version()}")
+        log("LXC Value: ${getGlobalConfiguration("lxc.lxcpath")}")
     }
     launch()
 }

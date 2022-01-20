95af2160598707fa809539d039ad10d4068ee1bd
anton.bashirov <anton.sh.local@gmail.com>
2022-01-20 07:00:53 +0300

    ART

--- container/src/main/kotlin/io/art/linux/local/container/Main.kt
+++ container/src/main/kotlin/io/art/linux/local/container/Main.kt
@@ -2,22 +2,21 @@ package io.art.linux.local.container
 
 import io.art.configurator.kotlin.configurator
 import io.art.core.checker.TerminalChecker.terminalSupportColors
+import io.art.core.context.Context.context
 import io.art.launcher.kotlin.activator
-import io.art.linux.local.container.graal.GraalLxcProvider
-import io.art.linux.local.container.graal.GraalLxcProvider.LxcContainer
-import io.art.linux.local.container.graal.GraalLxcProvider.getGlobalConfiguration
+import io.art.linux.local.container.graal.GraalLxcProvider.version
 import io.art.logging.colorizer.AnsiColorizer
 import io.art.logging.colorizer.AnsiColorizer.additional
 import io.art.logging.colorizer.AnsiColorizer.success
 import io.art.scheduler.kotlin.scheduler
 import io.art.transport.kotlin.transport
 
-fun log(message: String) = when {
+fun printOutput(message: String) = when {
     terminalSupportColors() -> println(additional("(art.local): ") + success(message))
     else -> println("(art.local): $message")
 }
 
-fun error(message: String) = when {
+fun printError(message: String) = when {
     terminalSupportColors() -> println(additional("(art.local): ") + AnsiColorizer.error(message))
     else -> println("(art.local): $message")
 }
@@ -27,9 +26,21 @@ fun main() = activator {
     scheduler()
     configurator()
     onLaunch {
-        log("Hello, ART with lxc: ${GraalLxcProvider.version()}")
-        log("LXC Value: ${getGlobalConfiguration("lxc.lxcpath")}")
-        log("LXC Container with name connect.local defined: ${LxcContainer("connect.local").defined()}")
+        val arguments = context().configuration().arguments
+
+        if (arguments.size() == 0) {
+            printOutput("ART Version: main")
+            printOutput("LXC Version: ${version()}")
+            return@onLaunch
+        }
+
+        if (arguments.size() == 1 && arguments[0] == "version") {
+            printOutput("ART Version: main")
+            printOutput("LXC Version: ${version()}")
+            return@onLaunch
+        }
+
+        printError("Wrong arguments count. Please refer to: https://animego.org/ for some brains")
     }
     launch()
 }

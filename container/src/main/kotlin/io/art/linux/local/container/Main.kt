4e21fce8781ca1fc59650b0dc641bb47579636b9
anton.bashirov <anton.sh.local@gmail.com>
2022-01-20 07:05:38 +0300

    ART

--- container/src/main/kotlin/io/art/linux/local/container/Main.kt
+++ container/src/main/kotlin/io/art/linux/local/container/Main.kt
@@ -40,7 +40,7 @@ fun main(arguments: Array<String>) = activator(arguments) {
             return@onLaunch
         }
 
-        printError("Wrong arguments count. Please go to: https://animego.org/ for some brains")
+        printError("Wrong arguments. Please go to: https://animego.org/ for some brains")
     }
     launch()
 }

852726ca8664227cae5d4c12ee1c5d356dc9687a
anton.bashirov <anton.sh.local@gmail.com>
2022-01-20 05:59:25 +0300

    ART

--- container/src/main/kotlin/io/art/linux/local/container/service/LxcService.kt
+++ container/src/main/kotlin/io/art/linux/local/container/service/LxcService.kt
@@ -2,6 +2,7 @@ package io.art.linux.local.container.service
 
 import io.art.linux.local.container.graal.GraalLxc.lxc_get_global_config_item
 import io.art.linux.local.container.graal.GraalLxc.lxc_get_version
+import org.graalvm.nativeimage.c.type.CCharPointer
 import org.graalvm.nativeimage.c.type.CTypeConversion.toCString
 import org.graalvm.nativeimage.c.type.CTypeConversion.toJavaString
 
@@ -13,7 +14,7 @@ object LxcService {
     object Configuration {
         operator fun get(key: String): String {
             val transformedKey = toCString(key)
-            val keyPointer = transformedKey.get()
+            val keyPointer: CCharPointer = transformedKey.get()
             return toJavaString(lxc_get_global_config_item(keyPointer))
         }
     }

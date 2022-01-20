f31213a2974e9c13e97b50eb58f3831030c09f87
anton.bashirov <anton.sh.local@gmail.com>
2022-01-20 06:10:23 +0300

    ART

--- container/src/main/kotlin/io/art/linux/local/container/service/LxcService.kt
+++ container/src/main/kotlin/io/art/linux/local/container/service/LxcService.kt
@@ -2,6 +2,7 @@ package io.art.linux.local.container.service
 
 import io.art.linux.local.container.graal.GraalLxc.lxc_get_global_config_item
 import io.art.linux.local.container.graal.GraalLxc.lxc_get_version
+import org.graalvm.nativeimage.c.type.CCharPointer
 import org.graalvm.nativeimage.c.type.CTypeConversion.toCString
 import org.graalvm.nativeimage.c.type.CTypeConversion.toJavaString
 
@@ -9,7 +10,8 @@ object LxcService {
     fun version(): String = toJavaString(lxc_get_version())
 
     fun getGlobalConfiguration(key: String): String = toCString(key).use { pin ->
-        val pinValue = pin.get()
-        return toJavaString(lxc_get_global_config_item(pinValue))
+        val pinValue: CCharPointer = pin.get() as CCharPointer
+        val itemValue: CCharPointer = lxc_get_global_config_item(pinValue) as CCharPointer
+        return toJavaString(itemValue) as String
     }
 }
